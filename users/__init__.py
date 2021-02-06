f6fefec4ec677e79612e9c83421babfa6373c1d2
HristoP96 <HPopov@infragistics.com>
2021-02-06 23:01:16 +0200

    Initial commit

---
+++ users/__init__.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+db_engine = create_engine('mysql://root:root@localhost:3306/users')
+Session = sessionmaker(bind=db_engine)
+Base = declarative_base()
