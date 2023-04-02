95c87a092428e6e4c9a5e0b12a44883187c2d72e
HrisoP <poh1sf3@bosch.com>
2023-04-02 21:45:03 +0300

    add docker config

--- invoices/__init__.py
+++ invoices/__init__.py
@@ -2,6 +2,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-db_engine = create_engine('postgresql+psycopg2://root:root@localhost:5432/dynobay_db')
+db_engine = create_engine('postgresql+psycopg2://root:root@localhost5431/rfm_db')
 Session = sessionmaker(bind=db_engine)
 Base = declarative_base()
